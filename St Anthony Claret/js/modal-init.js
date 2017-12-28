9af5469482a91e8e278ce2c06607f5014e5a044c
Eduardo Pelaez <eduardopelaez98@gmail.com>
2017-12-28 11:00:44 -0800

    More organization...

--- St Anthony Claret/js/modal-init.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var $content = $('#about-options').detach();
-    
-    $('#about').on('click', function() {
-        modal.open({content: $content, width: 500, height: 400});
-    });
-}());
