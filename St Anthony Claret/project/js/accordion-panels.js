22d2ae09e6dd82c395d52447009466f68247437e
Eduardo Pelaez <eduardopelaez98@gmail.com>
2018-05-16 00:20:36 -0700

    Update design for Udacity's portfolio project

--- St Anthony Claret/project/js/accordion-panels.js
+++
@@ -1,8 +0,0 @@
-$('.accordion-panel:first').show();
-$('.accordion').on('click', '.accordion-control', function(e) {
-    e.preventDefault();
-    $(this)
-        .next('.accordion-panel')
-        .not(':animated')
-        .slideToggle();
-});
